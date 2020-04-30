5090aea94d3afdba4e89129b3f3b0bdc8741be31
arneVG <arnevangestel@gmail.com>
2020-04-30 18:49:25 +0200

    merged files into one script

--- budgiebackup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/budgiebackup
-
-dconf dump /com/solus-project/ > ~/budgiebackup/budgie-dconf-dump
-
-tar -czvf budgiebackup.tar.gz budgiebackup/
-
-rm -R ~/budgiebackup/
--- budgierestore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/budgiebackup/
-
-tar -xzvf budgiebackup.tar.gz
-
-dconf load /com/solus-project/ < ~/budgiebackup/budgie-dconf-dump
-
-rm -R ~/budgiebackup/
